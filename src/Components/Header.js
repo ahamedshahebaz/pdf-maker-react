1cbbbe2850c7c0cc590946f97f28fe7c9ca10918
ahamedshahebaz <144301976+ahamedshahebaz@users.noreply.github.com>
2023-12-07 22:25:14 -0600

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -79,7 +79,7 @@ const Header = (props) => {
     const file = event.target.files[0];
     if (file) {
       // Check if the file type is a Word document
-      if (file.type === 'application/msword' || file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+      if (file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
         // File is a Word document
         setSelectedFile(file);
         setFileName(file.name);
